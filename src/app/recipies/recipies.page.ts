797c81fd5ca4b6950becf78aebf53d6235ccd03f
fahim zuhair <fahim.zuhair@opencyon.com>
2019-10-24 15:19:14 +0530

    created list item

--- src/app/recipies/recipies.page.ts
+++ src/app/recipies/recipies.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipies',
   templateUrl: './recipies.page.html',
-  styleUrls: ['./recipies.page.scss'],
+  styleUrls: ['./recipies.page.scss']
 })
 export class RecipiesPage implements OnInit {
+  recipies: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Recipie 1',
+      imageUrl: 'https://picsum.photos/200',
+      ingredients: ['ing1 of r1', 'ing2 of r1']
+    },
+    {
+      id: 'r2',
+      title: 'Recipie 2',
+      imageUrl: 'https://picsum.photos/200',
+      ingredients: ['ing1 of r2', 'ing2 of r2']
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
