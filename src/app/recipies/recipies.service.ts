89c1092fcf75a34ccf7179a79054aa973713f0d0
fahim zuhair <fahim.zuhair@opencyon.com>
2019-10-24 16:15:16 +0530

    updated recipies service

--- src/app/recipies/recipies.service.ts
+++ src/app/recipies/recipies.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipiesService {
-
-  constructor() { }
+  private recipies: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Recipie 1',
+      imageUrl: 'https://picsum.photos/200',
+      ingredients: ['ing1 of r1', 'ing2 of r1']
+    },
+    {
+      id: 'r2',
+      title: 'Recipie 2',
+      imageUrl: 'https://picsum.photos/400',
+      ingredients: ['ing1 of r2', 'ing2 of r2']
+    }
+  ];
+  constructor() {}
+  getAllRecipies() {
+    return [...this.recipies];
+  }
+  getRecipie(recipieId: string) {
+    return {
+      ...this.recipies.find(recipie => {
+        return recipie.id === recipieId;
+      })
+    };
+  }
 }
